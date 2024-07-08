38bc86e95236714e12c823a4cf5af02cfcf21c59
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2024-07-08 08:15:46 +0530

    Create variables-data-types.js

---
+++ variables-data-types.js
@@ -0,0 +1,15 @@
+// Variables and Data Types
+
+// Declaring variables
+var x = 5; // var
+let y = 10; // let
+const z = 15; // const
+
+// Data types
+let string = "Hello, World!"; // String
+let number = 123; // Number
+let boolean = true; // Boolean
+let array = [1, 2, 3, 4, 5]; // Array
+let object = { name: "John", age: 30 }; // Object
+
+console.log(x, y, z, string, number, boolean, array, object);
