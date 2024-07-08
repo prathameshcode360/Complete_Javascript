d5347162877c909999d303326a0604d26704680a
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2024-07-08 08:16:29 +0530

    Create operators.js

---
+++ operators.js
@@ -0,0 +1,22 @@
+// Operators
+
+let a = 10;
+let b = 5;
+
+// Arithmetic operators
+console.log(a + b); // Addition
+console.log(a - b); // Subtraction
+console.log(a * b); // Multiplication
+console.log(a / b); // Division
+console.log(a % b); // Modulus
+
+// Comparison operators
+console.log(a == b); // Equal
+console.log(a != b); // Not equal
+console.log(a > b); // Greater than
+console.log(a < b); // Less than
+
+// Logical operators
+console.log(a > 0 && b > 0); // AND
+console.log(a > 0 || b > 0); // OR
+console.log(!(a > 0)); // NOT
