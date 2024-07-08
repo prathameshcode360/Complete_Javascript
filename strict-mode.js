17e3b721580b1a6d6b634163bccfe88a834fa5f4
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2024-07-08 08:33:55 +0530

    Create strict-mode.js

---
+++ strict-mode.js
@@ -0,0 +1,11 @@
+// Strict Mode
+
+"use strict";
+
+function strictFunction() {
+    // x = 3.14; // This will cause an error because x is not declared
+    let x = 3.14;
+    console.log(x);
+}
+
+strictFunction();
