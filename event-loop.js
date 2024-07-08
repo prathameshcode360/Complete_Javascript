92a558220768e99e9a3be718204ce5451940d69f
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2024-07-08 08:28:06 +0530

    Create event-loop.js

---
+++ event-loop.js
@@ -0,0 +1,13 @@
+// Event Loop
+
+console.log('Start');
+
+setTimeout(() => {
+    console.log('Timeout');
+}, 0);
+
+Promise.resolve().then(() => {
+    console.log('Promise');
+});
+
+console.log('End');
