87c8cab36eda0410dc8d4d95b2c84dfd17b6e647
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2024-07-08 08:27:24 +0530

    Create fetch-api.js

---
+++ fetch-api.js
@@ -0,0 +1,6 @@
+// Fetch API
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
