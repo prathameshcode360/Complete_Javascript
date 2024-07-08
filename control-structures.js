efa013979c21cc65a7fe7781287ef94d5ff5d9c2
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2024-07-08 08:17:44 +0530

    Create control-structures.js

---
+++ control-structures.js
@@ -0,0 +1,34 @@
+// Control Structures
+
+let num = 10;
+
+// if-else statement
+if (num > 0) {
+    console.log("Number is positive");
+} else {
+    console.log("Number is negative");
+}
+
+// switch statement
+switch (num) {
+    case 1:
+        console.log("Number is one");
+        break;
+    case 10:
+        console.log("Number is ten");
+        break;
+    default:
+        console.log("Number is neither one nor ten");
+}
+
+// for loop
+for (let i = 0; i < 5; i++) {
+    console.log("For loop iteration: " + i);
+}
+
+// while loop
+let i = 0;
+while (i < 5) {
+    console.log("While loop iteration: " + i);
+    i++;
+}
